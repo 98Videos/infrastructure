2aa514f1876d28096b69174cc6f88b3f3d5d77f9
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2025-01-26 14:39:45 -0300

    add buckets for each service

--- backend_config.tf
+++ backend_config.tf
@@ -1,6 +1,13 @@
-terraform {
-  backend "s3" {
-    key    = "states/kubernetes-terraform"
-    region = "us-east-1"
+resource "aws_s3_bucket" "videos_bucket" {
+  bucket = "videos-bucket-soat72025"
+  tags = {
+    Name = "VideosBucket"
+  }
+}
+
+resource "aws_s3_bucket" "zip_images_bucket" {
+  bucket = "zip-images-bucket-soat72025"
+  tags = {
+    Name = "ZipImagesBucket"
   }
 }
