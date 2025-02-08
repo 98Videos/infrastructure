560b741b0e1cba75b137d3d68a5d665d541c1cd9
Andre Torres <atcorrea17@gmail.com>
2025-02-07 23:03:57 -0300

    chore: adapt vpc and eks for k8s lb

--- rds_postgresql.tf
+++ rds_postgresql.tf
@@ -1,31 +1,17 @@
-data "aws_subnets" "private_subnets" {
-  depends_on = [aws_subnet.private-us-east-1a, aws_subnet.private-us-east-1b]
-  filter {
-    name   = "tag:type"
-    values = ["private"]
-  }
-}
-
-data "aws_security_groups" "eks_security_groups" {
-  filter {
-    name   = "tag:aws:eks:cluster-name"
-    values = ["dotcluster"]
-  }
-}
-
 resource "aws_db_subnet_group" "postgresql_rds_subnet_group" {
   name       = "postgresql_rds-subnet-group"
-  subnet_ids = data.aws_subnets.private_subnets.ids
+  subnet_ids = [aws_subnet.private-us-east-1a.id, aws_subnet.private-us-east-1b.id]
 }
 
 resource "aws_db_instance" "media-management-db" {
+  depends_on              = [aws_eks_cluster.dotcluster]
   identifier              = "dotvideos-pgsql"
   db_name                 = "dotvideosMediaManagement"
   engine                  = "postgres"
   instance_class          = "db.t4g.micro"
   username                = var.mediamanagement_dbuser
   password                = var.mediamanagement_dbpassword
-  vpc_security_group_ids  = data.aws_security_groups.eks_security_groups.ids
+  vpc_security_group_ids  = [aws_security_group.eks_security_group.id]
   db_subnet_group_name    = aws_db_subnet_group.postgresql_rds_subnet_group.name
   allocated_storage       = 20
   skip_final_snapshot     = true
