1aed83a747303b3f91d9aa3d273b67c699908f10
Andre Torres <atcorrea17@gmail.com>
2025-02-08 19:30:22 -0300

    fix: remove unecessary file

--- vars.ps1
+++
@@ -1,4 +0,0 @@
-$env:TF_INPUT="false"
-$env:TF_VAR_eks_role="arn:aws:iam::749021552279:role/LabRole"
-$env:TF_VAR_mediamanagement_dbuser="mediaManagementUser"
-$env:TF_VAR_mediamanagement_dbpassword="6X&jO'0Ff&8_"
